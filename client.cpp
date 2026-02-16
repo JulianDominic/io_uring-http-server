0c4445e314798505b178678d4642e047117f490d
Julian <juliandominic@yahoo.com.sg>
2026-02-16 15:52:31 +0800

    fix: let the client receive the bytes from the socket

--- client.cpp
+++ client.cpp
@@ -4,20 +4,26 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+char buffer[1024];
+
 int main(void) {
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("failed to connect to the server\n");
+        return 1;
+    }
     const char* message = "i'm the client";
     send(clientSocket, message, strlen(message), 0);
-
-    accept(clientSocket, nullptr, nullptr);
-
-    char buffer[1024];
-    recv(clientSocket, buffer, strlen(buffer), 0);
-    std::cout << "Server response: " << buffer << std::endl;
+    while (true) {
+        int bytes_recv = recv(clientSocket, buffer, 2048, 0);
+        if (bytes_recv > 0) {
+            std::cout << "Server response: " << buffer << std::endl;
+            break;        
+        }
+    }
     close(clientSocket);
 }
