79cbe4ab8cf224c75ab9d24d2849e08b1e542538
JulianDominic <juliandominic@yahoo.com.sg>
2026-02-16 11:03:10 +0800

    feat: get port number as an argument

--- server.cpp
+++ server.cpp
@@ -3,29 +3,42 @@
 #include <sys/socket.h>
 // #include <unistd.h>
 
-const int MAX_CONNECTIONS = 5;
-
+#define MAX_CONNECTIONS 4096
 #define EXT_ERR_INIT_SOCKET 1
 #define EXT_ERR_INIT_LISTEN 2
 #define EXT_ERR_INIT_ACCEPT 3
+#define EXT_ERR_INIT_BIND 4
+#define EXT_ERR_PORT_MISSING 5
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Port number required: ./server <port>" << std::endl;
+        return EXT_ERR_PORT_MISSING;
+    }
     // create socket
     // AF_INET: IPv4; SOCK_STREAM: TCP socket
     int server = socket(AF_INET, SOCK_STREAM, 0);
     if (server == -1) {
         return EXT_ERR_INIT_SOCKET;
     }
+    const int value = 1;
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value));
+
     // bind the socket to a port
+    int port = strtol(argv[1], NULL, 10);
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080); // convert 8080 to network byte order
+    serverAddr.sin_port = htons(port); // convert 8080 to network byte order
     serverAddr.sin_addr.s_addr = INADDR_ANY; // accept connection on any IP
-    bind(server, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (bind(server, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        return EXT_ERR_INIT_BIND;
+    }
     // listen on the socket
     if (listen(server, MAX_CONNECTIONS) == -1) {
         return EXT_ERR_INIT_LISTEN;
     }
+    std::cout << "server is now listening on port: " << port << std::endl;
+
     while (true) {
         // accept connection
         int client = accept(server, nullptr, nullptr);
