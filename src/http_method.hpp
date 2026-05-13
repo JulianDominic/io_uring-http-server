2c5a07fdde1de3ca380445bc54938343fcc5bb31
JulianDominic <juliandominic@yahoo.com.sg>
2026-05-13 23:36:57 +0800

    refactor: only keep GET

--- src/http_method.hpp
+++ src/http_method.hpp
@@ -1,23 +1,12 @@
 #pragma once
-#include <ostream>
 #include <string>
 #include <unordered_map>
 
 enum class HTTPMethod {
     UNKNOWN,
     GET,
-    POST,
 };
 
 inline std::unordered_map<std::string, HTTPMethod> method_str_to_enum{
     { "GET", HTTPMethod::GET },
-    { "POST", HTTPMethod::POST },
 };
-
-inline std::ostream& operator<<(std::ostream& os, const HTTPMethod& method) {
-    switch (method) {
-        case HTTPMethod::GET: return os << "GET";
-        case HTTPMethod::POST: return os << "POST";
-        default: return os << "UNKNOWN";
-    }
-}
